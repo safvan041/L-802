8dac80689cc3d8c1c37128ca7acee815ed9abd45
BAKKAR SAFAWAN <127715574+safvan041@users.noreply.github.com>
2025-10-01 08:57:06 +0000

    react & vue practices

---
+++ React-learning/01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Test from "./test"
+
+function App() {
+ 
+
+  return (
+    <Test />
+  )
+}
+
+export default App
---
+++ React-learning/01vitereact/src/test.jsx
@@ -0,0 +1,8 @@
+function Test(){
+    return(
+        <h3>
+            test site is ready!
+        </h3>
+    )
+}
+export default Test
